3e161ecbbf005684a0a5a900cbbba426150141a0
purpen <purpen.w@gmail.com>
2013-04-23 14:59:25 +0800

    add production.

--- config/settings.js
+++ config/settings.js
@@ -1,20 +1,29 @@
 module.exports = {
     development: {
-      root: require('path').normalize(__dirname + '/..'),
-      app: {
-        name: '大牙兔'
-      },
-      db: 'mongodb://localhost/dayatu',
-      facebook: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:3000/auth/facebook/callback"
-      }
-    }
+		root: require('path').normalize(__dirname + '/..'),
+		app: {
+			name: '大牙兔'
+		},
+		db: 'mongodb://localhost/dayatu',
+		facebook: {
+			clientID: "APP_ID"
+			, clientSecret: "APP_SECRET"
+			, callbackURL: "http://localhost:3000/auth/facebook/callback"
+		}
+	}
   , test: {
 
     }
   , production: {
-
+		root: require('path').normalize(__dirname + '/..'),
+		app: {
+			name: '大牙兔'
+		},
+		db: 'mongodb://localhost/dayatu',
+		facebook: {
+			clientID: "APP_ID"
+			, clientSecret: "APP_SECRET"
+			, callbackURL: "http://localhost:3000/auth/facebook/callback"
+		}
     }
 }
