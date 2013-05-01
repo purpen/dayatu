4ce371a83de2a4676dd30091d3f50120ef977513
purpen <purpen.w@gmail.com>
2013-05-01 23:49:41 +0800

    add upload validate.

--- app/controllers/stuffs.js
+++ app/controllers/stuffs.js
@@ -46,16 +46,32 @@ exports.upload = function(req, res) {
  * Upload image to save.
  */
 exports.save = function(req, res) {
-	var stuff = new Stuff(req.body)
-	stuff.uploadAndSave(req.files.image, function(err){
-		if(err){
-			console.log(err)
-			res.render('stuffs/upload', {
-				stuff: stuff,
-				errors: err.errors
-			})
-		}else{
-			res.redirect('/stuffs/'+stuff.id)
-		}
-	})
+	var stuff = new Stuff(req.body),is_error=false,msg=[]
+	if (!req.body.title || req.body.title.length < 5) {
+		is_error = true
+		msg.push('标题不能为空并不少于5个字符')
+	}
+	if (req.files.image[0].size == 0){
+		is_error = true
+		msg.push('必须上传一张图片')
+	}
+	// is_error true
+	if (is_error) {
+		res.render('stuffs/upload', {
+			stuff: stuff,
+			errors: msg
+		})
+	}else{
+		stuff.uploadAndSave(req.files.image, function(err){
+			if(err){
+				console.log(err)
+				res.render('stuffs/upload', {
+					stuff: stuff,
+					errors: err.errors
+				})
+			}else{
+				res.redirect('/stuffs/'+stuff.id)
+			}
+		})
+	}
 }
